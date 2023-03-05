f17d0e1f09135b75bc248a209a704074a058355f
Michael Salim <salim_michaelmi@yahoo.com>
2023-03-05 00:06:48 +0000

    Fix: Deps get

--- repos/mirego__accent/script.sh
+++ repos/mirego__accent/script.sh
@@ -4,7 +4,7 @@ apk add yaml-dev
 git clone https://github.com/mirego/accent repo
 cd repo
 
-mix deps.get --only prod
+mix deps.get
 DATABASE_URL=postgresql://postgres:password@localhost/postgres mix ecto.migrate
 
 cd ..
