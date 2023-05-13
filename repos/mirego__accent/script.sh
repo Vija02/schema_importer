69da3dc6bec26468845c973e3aa0c3ea75468ca2
Michael Salim <salim_michaelmi@yahoo.com>
2023-05-13 20:40:38 +0100

    Chore: Try fix script

--- repos/mirego__accent/script.sh
+++ repos/mirego__accent/script.sh
@@ -6,8 +6,8 @@ apk add yaml-dev
 git clone https://github.com/mirego/accent repo
 cd repo
 
-mix deps.get
-DATABASE_URL=postgresql://postgres:password@localhost/postgres mix ecto.migrate
+make dependencies
+DATABASE_URL=postgresql://postgres:password@localhost/postgres mix ecto.setup
 
 cd ..
 cp /helpers/schema.prisma ./
