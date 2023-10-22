951be71967638679598b91e0911347104ea9e9e7
Michael Salim <salim_michaelmi@yahoo.com>
2023-10-23 00:41:58 +0100

    Feat: SQL-postgresql

--- repos/glific__glific/script.sh
+++ repos/glific__glific/script.sh
@@ -2,5 +2,4 @@
 git clone https://github.com/glific/glific repo
 cd repo
 
-MIX_ENV=prod mix deps.get --force
-MIX_ENV=prod DATABASE_NAME=postgres DATABASE_USER=postgres DATABASE_HOST=localhost DATABASE_PORT=5432 DATABASE_PASSWORD=postgres DATABASE_URL=postgresql://postgres:password@localhost/postgres mix ecto.setup
+psql postgresql://postgres:password@localhost/postgres -f priv/repo/structure.sql
