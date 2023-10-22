d40f37559eb20c3b00c9a9d2569ffa1ea3eda064
Michael Salim <salim_michaelmi@yahoo.com>
2023-10-23 00:12:49 +0100

    Chore: Mix env

--- repos/glific__glific/script.sh
+++ repos/glific__glific/script.sh
@@ -2,5 +2,5 @@
 git clone https://github.com/glific/glific repo
 cd repo
 
-mix deps.get --force
-DATABASE_NAME=postgres DATABASE_USER=postgres DATABASE_HOST=localhost DATABASE_PORT=5432 DATABASE_PASSWORD=postgres DATABASE_URL=postgresql://postgres:password@localhost/postgres mix ecto.setup
+MIX_ENV=prod mix deps.get --force
+MIX_ENV=prod DATABASE_NAME=postgres DATABASE_USER=postgres DATABASE_HOST=localhost DATABASE_PORT=5432 DATABASE_PASSWORD=postgres DATABASE_URL=postgresql://postgres:password@localhost/postgres mix ecto.setup
