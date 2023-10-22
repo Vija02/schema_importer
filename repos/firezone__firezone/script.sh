3a7775201bf1b098f8357dbbe929f363570fb949
Michael Salim <salim_michaelmi@yahoo.com>
2023-10-22 22:31:49 +0100

    Feat: Both env style

--- repos/firezone__firezone/script.sh
+++ repos/firezone__firezone/script.sh
@@ -4,4 +4,4 @@ cd repo
 cd elixir
 
 mix deps.get --force
-DATABASE_URL=postgresql://postgres:password@localhost/postgres mix ecto.setup
+DATABASE_NAME=postgres DATABASE_USER=postgres DATABASE_HOST=localhost DATABASE_PORT=5432 DATABASE_PASSWORD=postgres DATABASE_URL=postgresql://postgres:password@localhost/postgres mix ecto.setup
--- repos/mirego__accent/script.sh
+++ repos/mirego__accent/script.sh
@@ -5,4 +5,4 @@ git clone https://github.com/mirego/accent repo
 cd repo
 
 mix deps.get --force
-DATABASE_URL=postgresql://postgres:password@localhost/postgres mix ecto.setup
+DATABASE_NAME=postgres DATABASE_USER=postgres DATABASE_HOST=localhost DATABASE_PORT=5432 DATABASE_PASSWORD=postgres DATABASE_URL=postgresql://postgres:password@localhost/postgres mix ecto.setup
