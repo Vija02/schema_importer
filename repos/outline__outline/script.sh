18eae146916cf4c3d011478cbe2362666521d921
Michael Salim <salim_michaelmi@yahoo.com>
2024-01-07 20:16:35 +0000

    Feat: First sequelize repo

---
+++ repos/outline__outline/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+git clone https://github.com/outline/outline repo
+
+cd repo
+
+if [ -f yarn.lock ]; then
+  yarn
+else
+  npm install
+fi
+
+DATABASE_NAME=postgres DATABASE_USER=postgres DATABASE_HOST=localhost DATABASE_PORT=5432 DATABASE_PASSWORD=postgres DATABASE_URL=postgresql://postgres:password@localhost/postgres yarn db:migrate
