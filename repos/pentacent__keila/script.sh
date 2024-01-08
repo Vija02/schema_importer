e52a9308c1608a45a49d0addc575c653109d5e78
Michael Salim <salim_michaelmi@yahoo.com>
2024-01-08 14:33:47 +0000

    Feat: Add keila

---
+++ repos/pentacent__keila/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+git clone https://github.com/pentacent/keila repo
+cd repo
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=d
+export DB_URL=postgresql://postgres:password@localhost/postgres
+export MAILER_SMTP_HOST=d
+export MAILER_SMTP_USER=d
+export MAILER_SMTP_PASSWORD=d
+
+mix deps.get --force
+DATABASE_NAME=postgres DATABASE_USER=postgres DATABASE_HOST=localhost DATABASE_PORT=5432 DATABASE_PASSWORD=postgres DATABASE_URL=postgresql://postgres:password@localhost/postgres mix ecto.setup
