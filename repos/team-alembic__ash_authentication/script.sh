620c21a5eb6688a424c7178ac5b9e072122301f7
Michael Salim <salim_michaelmi@yahoo.com>
2024-01-14 23:35:41 +0000

    Feat: Add repo 'team-alembic/ash_authentication'

---
+++ repos/team-alembic__ash_authentication/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+git clone https://github.com/team-alembic/ash_authentication repo
+cd repo
+
+export DATABASE_NAME=postgres 
+export DATABASE_USER=postgres 
+export DATABASE_HOST=localhost 
+export DATABASE_PORT=5432 
+export DATABASE_PASSWORD=postgres 
+export DATABASE_URL=postgresql://postgres:password@localhost/postgres 
+
+# export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+ash_authentication_dev
+sed -i -e "s/ash_authentication_dev/postgres/g" config/dev.exs
+
+mix deps.get --force
+mix ecto.create
+mix ecto.migrate
