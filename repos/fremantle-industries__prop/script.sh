0c6e053c0609098b42f5475211802e950ea3cf68
Michael Salim <salim_michaelmi@yahoo.com>
2024-01-14 20:42:22 +0000

    Feat: Add repo 'fremantle-industries/prop'

---
+++ repos/fremantle-industries__prop/script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+git clone https://github.com/fremantle-industries/prop repo
+cd repo
+
+curl https://sh.rustup.rs -sSf | bash -s -- -y
+source "$HOME/.cargo/env"
+
+export DATABASE_NAME=postgres 
+export DATABASE_USER=postgres 
+export DATABASE_HOST=localhost 
+export DATABASE_PORT=5432 
+export DATABASE_PASSWORD=postgres 
+export DATABASE_URL=postgresql://postgres:password@localhost/postgres 
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+
+mix deps.get --force
+mix deps.update ecto_sql ex_keccak
+mix ecto.setup
