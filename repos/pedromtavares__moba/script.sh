15df5d5ad0f66fe117e85143ac1070d021af4e8e
Michael Salim <salim_michaelmi@yahoo.com>
2024-01-14 20:42:22 +0000

    Feat: Add repo 'pedromtavares/moba'

---
+++ repos/pedromtavares__moba/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+git clone https://github.com/pedromtavares/moba repo
+cd repo
+
+export DATABASE_NAME=postgres 
+export DATABASE_USER=postgres 
+export DATABASE_HOST=localhost 
+export DATABASE_PORT=5432 
+export DATABASE_PASSWORD=postgres 
+export DATABASE_URL=postgresql://postgres:password@localhost/postgres 
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+
+mix deps.get --force
+mix ecto.setup
