f4781f9339e3a5b2367bf503703e5ada7e1002e6
Michael Salim <salim_michaelmi@yahoo.com>
2024-01-14 20:42:22 +0000

    Feat: Add repo 'warronbebster/mayor_game'

---
+++ repos/warronbebster__mayor_game/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+git clone https://github.com/warronbebster/mayor_game repo
+cd repo
+
+export DATABASE_NAME=postgres 
+export DATABASE_USER=postgres 
+export DATABASE_HOST=localhost 
+export DATABASE_PORT=5432 
+export DATABASE_PASSWORD=postgres 
+export DATABASE_URL=postgresql://postgres:password@localhost/postgres 
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+export MAIL_SECRET=d
+
+mix deps.get --force
+mix ecto.setup
