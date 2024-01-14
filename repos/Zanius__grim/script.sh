009d3d5443c77a609d5be42113c9a1aa16066865
Michael Salim <salim_michaelmi@yahoo.com>
2024-01-14 22:42:22 +0000

    Feat: Add repo 'Zanius/grim'

---
+++ repos/Zanius__grim/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+git clone https://github.com/Zanius/grim repo
+cd repo
+
+export DATABASE_NAME=postgres 
+export DATABASE_USER=postgres 
+export DATABASE_HOST=localhost 
+export DATABASE_PORT=5432 
+export DATABASE_PASSWORD=postgres 
+export DATABASE_URL=postgresql://postgres:password@localhost/postgres 
+
+export MIX_ENV=test
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+
+sed -i -e "s/grim_test/postgres/g" config/test.exs
+
+mix deps.get --force
+mix ecto.setup
