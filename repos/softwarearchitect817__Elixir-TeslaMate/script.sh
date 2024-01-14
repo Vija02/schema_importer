378ba27a65976796533fe8d77b364f0548d3da65
Michael Salim <salim_michaelmi@yahoo.com>
2024-01-14 20:42:22 +0000

    Feat: Add repo 'softwarearchitect817/Elixir-TeslaMate'

---
+++ repos/softwarearchitect817__Elixir-TeslaMate/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+git clone https://github.com/softwarearchitect817/Elixir-TeslaMate repo
+cd repo
+
+export DATABASE_NAME=postgres 
+export DATABASE_USER=postgres 
+export DATABASE_HOST=localhost 
+export DATABASE_PORT=5432 
+export DATABASE_PASSWORD=postgres 
+export DATABASE_PASS=postgres 
+export DATABASE_URL=postgresql://postgres:password@localhost/postgres 
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+export MQTT_HOST=d
+
+mix deps.get --force
+mix ecto.setup
