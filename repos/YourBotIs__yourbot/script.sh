54fa7bee98d10b70ed2c4030927620bd65880f07
Michael Salim <salim_michaelmi@yahoo.com>
2024-01-14 20:42:22 +0000

    Feat: Add repo 'YourBotIs/yourbot'

---
+++ repos/YourBotIs__yourbot/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+git clone https://github.com/YourBotIs/yourbot repo
+cd repo
+
+export DATABASE_NAME=postgres 
+export DATABASE_USER=postgres 
+export DATABASE_HOST=localhost 
+export DATABASE_PORT=5432 
+export DATABASE_PASSWORD=postgres 
+export DATABASE_URL=postgresql://postgres:password@localhost/postgres 
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+export API_TOKEN_SECRET=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+export AWS_ACCESS_KEY_ID=d
+export AWS_SECRET_ACCESS_KEY=d
+
+mix deps.get --force
+mix ecto.setup
