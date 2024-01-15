c099320e1d2c93f9d2b2ca1b027e6ab3c58a61ac
Michael Salim <salim_michaelmi@yahoo.com>
2024-01-15 00:41:39 +0000

    Feat: Add repo 'dgigafox/drill'

---
+++ repos/dgigafox__drill/script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+git clone https://github.com/dgigafox/drill repo
+cd repo
+
+export DATABASE_NAME=postgres 
+export DATABASE_USER=postgres 
+export DATABASE_HOST=localhost 
+export DATABASE_PORT=5432 
+export DATABASE_PASSWORD=postgres 
+export DATABASE_URL=postgresql://postgres:password@localhost/postgres 
+
+export MIX_ENV=test
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+
+cat <<EOT > config/test.exs
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run \`mix help test\` for more information.
+config :drill, Drill.Test.Repo,
+  username: "postgres",
+  password: "postgres",
+  database: "postgres",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+config :drill, ecto_repos: [Drill.Test.Repo]
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+EOT
+
+mix deps.get --force
+mix ecto.setup
