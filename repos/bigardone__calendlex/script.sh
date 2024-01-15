430ac7447bacdb64f8e3050dcc93ae3a90de19c5
Michael Salim <salim_michaelmi@yahoo.com>
2024-01-15 00:23:59 +0000

    Feat: Add repo 'bigardone/calendlex'

---
+++ repos/bigardone__calendlex/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+git clone https://github.com/bigardone/calendlex repo
+cd repo
+
+export DATABASE_NAME=postgres 
+export DATABASE_USER=postgres 
+export DATABASE_HOST=localhost 
+export DATABASE_PORT=5432 
+export DATABASE_PASSWORD=postgres 
+export DATABASE_URL=postgresql://postgres:password@localhost/postgres 
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+
+sed -i -e "s/ssl: true/ssl: false/g" config/runtime.exs
+
+mix deps.get --force
+mix ecto.setup
