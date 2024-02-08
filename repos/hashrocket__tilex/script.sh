ba6364b03937c9f2b08d81fa339770bc6c962db3
Michael Salim <salim_michaelmi@yahoo.com>
2024-02-08 16:19:12 +0000

    Feat: Add repo 'hashrocket/tilex'

---
+++ repos/hashrocket__tilex/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+git clone https://github.com/hashrocket/tilex repo
+cd repo
+
+export DATABASE_NAME=postgres 
+export DATABASE_USER=postgres 
+export DATABASE_HOST=localhost 
+export DATABASE_PORT=5432 
+export DATABASE_PASSWORD=postgres 
+export DATABASE_URL=postgresql://postgres:password@localhost/postgres 
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+
+sed -i -e "s/ssl: true/ssl: false/g" config/runtime.exs
+
+mix deps.get --force
+mix deps.update ecto_sql
+mix ecto.setup
