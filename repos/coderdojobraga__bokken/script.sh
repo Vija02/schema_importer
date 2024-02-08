71995385e7f893a77a191db4fb8207b549cf3624
Michael Salim <salim_michaelmi@yahoo.com>
2024-02-08 16:38:39 +0000

    Feat: Add repo 'coderdojobraga/bokken'

---
+++ repos/coderdojobraga__bokken/script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+git clone https://github.com/coderdojobraga/bokken repo
+cd repo
+
+export DB_NAME=postgres 
+export DB_USERNAME=postgres 
+export DB_HOST=localhost 
+export DB_PORT=5432 
+export DB_PASSWORD=postgres 
+export DATABASE_URL=postgresql://postgres:password@localhost/postgres 
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+export FRONTEND_URL=d
+export PORT=7070
+export SECRET_KEY_GUARDIAN=d
+export HOST_URL=d
+
+mix deps.get --force
+mix ecto.setup
