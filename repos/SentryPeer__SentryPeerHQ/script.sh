1e94296328959fe48d4aed060093fccce4f0cec3
Michael Salim <salim_michaelmi@yahoo.com>
2024-02-08 16:19:12 +0000

    Feat: Add repo 'SentryPeer/SentryPeerHQ'

---
+++ repos/SentryPeer__SentryPeerHQ/script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+git clone https://github.com/SentryPeer/SentryPeerHQ repo
+cd repo
+
+su postgres -c 'pg_ctl -D /var/lib/postgresql/data stop'
+
+curl -s https://packagecloud.io/install/repositories/timescale/timescaledb/script.deb.sh | bash
+apt-get install -y timescaledb-2-postgresql-16
+
+/usr/bin/timescaledb-tune --quiet --yes
+
+su postgres -c 'pg_ctl -D /var/lib/postgresql/data start'
+wait-until "psql -U postgres -c 'select 1'"
+
+export DATABASE_NAME=postgres 
+export DATABASE_USER=postgres 
+export DATABASE_HOST=localhost 
+export DATABASE_PORT=5432 
+export DATABASE_PASSWORD=postgres 
+export DATABASE_URL=postgresql://postgres:password@localhost/postgres 
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+export AUTH0_DOMAIN=d
+export AUTH0_AUDIENCE=d
+export AUTH0_CLIENT_ID=d
+export AUTH0_CLIENT_SECRET=d
+export FLY_APP_NAME=d
+
+mix deps.get --force
+mix ecto.setup
