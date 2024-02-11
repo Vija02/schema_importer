ef0b32726133ad1e3b51c17875d13c74edc66dd0
Michael Salim <salim_michaelmi@yahoo.com>
2024-02-11 21:50:43 +0000

    Feat: Add repo 'silbermm/distributed_sqlite'

---
+++ repos/silbermm__distributed_sqlite/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+git clone https://github.com/silbermm/distributed_sqlite repo
+cd repo
+
+export DATABASE_PATH=../database.db
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+export FLY_APP_NAME=d
+
+mix deps.get --force
+mix ecto.setup
