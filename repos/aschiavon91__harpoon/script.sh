c22489fc38020c28ac59e34cf4208c466ca0ad50
Michael Salim <salim_michaelmi@yahoo.com>
2024-02-11 21:50:45 +0000

    Feat: Add repo 'aschiavon91/harpoon'

---
+++ repos/aschiavon91__harpoon/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+git clone https://github.com/aschiavon91/harpoon repo
+cd repo
+
+export DATABASE_FILE=../database.db
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+
+mix deps.get --force
+mix ecto.setup
