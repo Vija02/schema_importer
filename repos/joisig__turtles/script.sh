277b74a17d52a0a5acb48ded87f471012f3a260b
Michael Salim <salim_michaelmi@yahoo.com>
2024-02-11 21:50:45 +0000

    Feat: Add repo 'joisig/turtles'

---
+++ repos/joisig__turtles/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+git clone https://github.com/joisig/turtles repo
+cd repo
+
+export DATABASE_PATH=../database.db
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+
+touch config/tconfig.secret.exs
+
+mix deps.get --force
+mix ecto.setup
