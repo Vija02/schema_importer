7656f8687c319e7b3ebf190f042d02962e3bb272
Michael Salim <salim_michaelmi@yahoo.com>
2024-02-11 21:50:45 +0000

    Feat: Add repo 'kubaryi/yabtt'

---
+++ repos/kubaryi__yabtt/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+git clone https://github.com/kubaryi/yabtt repo
+cd repo
+
+export DATABASE_PATH=../database.db
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+
+sed -i -e "s/\/var\/lib\/sqlite3\/yabtt.db/..\/database.db/g" config/config.exs
+
+mix deps.get --force
+mix deps.update ecto_sql
+mix ecto.setup
