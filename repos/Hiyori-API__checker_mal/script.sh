e2a4f79d3784aeec145616706601cee960f41223
Michael Salim <salim_michaelmi@yahoo.com>
2024-02-11 21:50:45 +0000

    Feat: Add repo 'Hiyori-API/checker_mal'

---
+++ repos/Hiyori-API__checker_mal/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+git clone https://github.com/Hiyori-API/checker_mal repo
+cd repo
+
+export DATABASE_PATH=../database.db
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+export MAL_CLIENTID=d
+export CHECKER_MAL_SECRET=d
+
+mix deps.get --force
+mix ecto.setup
