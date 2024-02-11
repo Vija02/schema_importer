a9627fc6f89ed3c561dbab6d122e27f2d8052ce1
Michael Salim <salim_michaelmi@yahoo.com>
2024-02-11 21:50:45 +0000

    Feat: Add repo 'jeregrine/hex-search'

---
+++ repos/jeregrine__hex-search/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+git clone https://github.com/jeregrine/hex-search repo
+cd repo
+
+export DATABASE_PATH=../database.db
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+
+mix deps.get --force
+mix ecto.migrate
