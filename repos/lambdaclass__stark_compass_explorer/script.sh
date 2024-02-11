b12bd371f13930f05e8bbb064cdea7e9de7513c8
Michael Salim <salim_michaelmi@yahoo.com>
2024-02-11 21:50:45 +0000

    Feat: Add repo 'lambdaclass/stark_compass_explorer'

---
+++ repos/lambdaclass__stark_compass_explorer/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+git clone https://github.com/lambdaclass/stark_compass_explorer repo
+cd repo
+
+export DATABASE_PATH=../database.db
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+export RPC_API_HOST=d
+export TESTNET_RPC_API_HOST=d
+export SEPOLIA_RPC_API_HOST=d
+export DB_TYPE=sqlite
+
+mix deps.get --force
+mix ecto.setup
