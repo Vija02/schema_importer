e4c058cddedd7c8d028c781d2e0b41081d4dd01b
Michael Salim <salim_michaelmi@yahoo.com>
2024-02-11 21:50:45 +0000

    Feat: Add repo 'recursiveGecko/race_bot'

---
+++ repos/recursiveGecko__race_bot/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+git clone https://github.com/recursiveGecko/race_bot repo
+cd repo
+
+export DATABASE_PATH=../database.db
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+export TWITTER_CONSUMER_KEY=d
+export TWITTER_CONSUMER_SECRET=d
+export TWITTER_ACCESS_TOKEN=d
+export TWITTER_ACCESS_TOKEN_SECRET=d
+export PHX_HOST=d
+export PORT=9999
+
+mix deps.get --force
+mix ecto.setup
