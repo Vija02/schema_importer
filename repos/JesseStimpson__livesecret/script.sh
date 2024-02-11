ec982e66df4fbcadd1956777489ff83877cd29f8
Michael Salim <salim_michaelmi@yahoo.com>
2024-02-11 21:50:44 +0000

    Feat: Add repo 'JesseStimpson/livesecret'

---
+++ repos/JesseStimpson__livesecret/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+git clone https://github.com/JesseStimpson/livesecret repo
+cd repo
+
+export DATABASE_PATH=../database.db
+
+export MIX_ENV=prod
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+
+export BEHIND_PROXY=1
+
+mix deps.get --force
+mix ecto.setup
