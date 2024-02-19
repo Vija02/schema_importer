3edbea526f3e0a4f0834da3afb4118e3b48323f7
Michael Salim <salim_michaelmi@yahoo.com>
2024-02-19 10:14:45 +0000

    Feat: Add repo 'hez/brew-dash'

---
+++ repos/hez__brew-dash/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+git clone --recursive https://github.com/hez/brew-dash repo
+cd repo
+
+export SQLITE_DB_FILE=../database.db
+
+export MIX_ENV=prod
+
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+export FLY_APP_NAME=d
+
+mix deps.get --force
+mix deps.update ecto_sql
+mix ecto.create && mix ecto.migrate
