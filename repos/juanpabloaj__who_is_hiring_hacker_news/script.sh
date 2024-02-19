3bb0511ee80eb61b218a1ea7674e9dbd0cb45468
Michael Salim <salim_michaelmi@yahoo.com>
2024-02-19 10:14:45 +0000

    Feat: Add repo 'juanpabloaj/who_is_hiring_hacker_news'

---
+++ repos/juanpabloaj__who_is_hiring_hacker_news/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+git clone --recursive https://github.com/juanpabloaj/who_is_hiring_hacker_news repo
+cd repo
+
+export MIX_ENV=prod
+
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+export FLY_APP_NAME=d
+export HN_PARENT_POST=d
+export TECHS_OF_INTEREST=d
+
+export DATABASE_PATH=../database.db
+
+mix deps.get --force
+mix deps.update ecto_sql
+mix ecto.create && mix ecto.migrate
