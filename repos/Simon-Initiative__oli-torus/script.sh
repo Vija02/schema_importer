aee9246647473044b8c508f87299e8f9d49da984
Michael Salim <salim_michaelmi@yahoo.com>
2024-02-19 10:14:45 +0000

    Feat: Add repo 'Simon-Initiative/oli-torus'

---
+++ repos/Simon-Initiative__oli-torus/script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+git clone --recursive https://github.com/Simon-Initiative/oli-torus repo
+cd repo
+
+export DATABASE_NAME=postgres 
+export DB_NAME=postgres 
+export DATABASE_USER=postgres 
+export DB_USER=postgres 
+export DATABASE_HOST=localhost 
+export DB_HOST=localhost 
+export DATABASE_PORT=5432 
+export DB_PORT=5432 
+export DATABASE_PASSWORD=postgres 
+export DB_PASSWORD=postgres 
+export DATABASE_URL=postgresql://postgres:password@localhost/postgres 
+export DB_URL=postgresql://postgres:password@localhost/postgres 
+
+export MIX_ENV=prod
+
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+export FLY_APP_NAME=d
+export LIVE_VIEW_SALT=d
+export HOST=d
+export S3_MEDIA_BUCKET_NAME=d
+export S3_XAPI_BUCKET_NAME=d
+export MEDIA_URL=d
+
+mix deps.get --force
+mix deps.update ecto_sql
+mix ecto.create && mix ecto.migrate
