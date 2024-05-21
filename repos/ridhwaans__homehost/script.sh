5ba767500bf934af610eeddc01a637bed628ec69
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-21 17:50:44 +0100

    Feat: Add repo 'ridhwaans/homehost'

---
+++ repos/ridhwaans__homehost/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+git clone https://github.com/ridhwaans/homehost repo
+cd repo
+
+export DATABASE_URL=../database.db
+export MIGRATION_FOLDER=packages/server/prisma
+
+find $MIGRATION_FOLDER -iname "*.sql" | sort | while read file; do
+  sqlite3 $DATABASE_URL < $file
+done
