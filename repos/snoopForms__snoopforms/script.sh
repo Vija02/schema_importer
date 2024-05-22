710ee83ce3c08cd7acf25c2081ee044c9732c3ee
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-22 11:05:11 +0100

    Feat: Add repo 'snoopForms/snoopforms'

---
+++ repos/snoopForms__snoopforms/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+git clone --recursive https://github.com/snoopForms/snoopforms repo
+cd repo
+
+export DATABASE_URL=postgresql://postgres:password@localhost/postgres
+export MIGRATION_FOLDER=packages/database/migrations
+
+find $MIGRATION_FOLDER -iname "*.sql" ! -iname "down.sql" | sort | xargs printf -- '-f %s\n' | xargs psql $DATABASE_URL -v ON_ERROR_STOP=1
