d1d94f42efcea6be3c19e42e95ce8207372d3ed5
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-22 11:05:11 +0100

    Feat: Add repo 'nueno-co/nueno'

---
+++ repos/nueno-co__nueno/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+git clone --recursive https://github.com/nueno-co/nueno repo
+cd repo
+
+export DATABASE_URL=postgresql://postgres:password@localhost/postgres
+export MIGRATION_FOLDER=prisma
+
+export SCHEMA_FOLDER=prisma
+mkdir -p $SCHEMA_FOLDER/migrations/0_init
+prisma migrate diff \
+--from-empty \
+--to-schema-datamodel $SCHEMA_FOLDER/schema.prisma \
+--script > $SCHEMA_FOLDER/migrations/0_init/migration.sql
+
+find $MIGRATION_FOLDER -iname "*.sql" ! -iname "down.sql" | sort | xargs printf -- '-f %s\n' | xargs psql $DATABASE_URL -v ON_ERROR_STOP=1
