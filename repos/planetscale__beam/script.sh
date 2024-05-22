08fc5cc8def1f3eaac4fdc32103804be9beaf582
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-22 15:34:25 +0100

    Feat: Add repo 'planetscale/beam'

---
+++ repos/planetscale__beam/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+git clone --recursive https://github.com/planetscale/beam repo
+cd repo
+
+export MIGRATION_FOLDER=prisma
+
+export SCHEMA_FOLDER=prisma
+mkdir -p $SCHEMA_FOLDER/migrations/0_init
+prisma migrate diff \
+--from-empty \
+--to-schema-datamodel $SCHEMA_FOLDER/schema.prisma \
+--script > $SCHEMA_FOLDER/migrations/0_init/migration.sql
+
+find $MIGRATION_FOLDER -iname "*.sql" ! -iname "down.sql" | sort | while read file; do
+  mariadb -uroot -ppassword db < $file
+done
