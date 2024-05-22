2baae5ebff7889a5e5b41501a1f0fa86220e79bd
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-22 11:59:24 +0100

    Feat: Add repo 'crusher-dev/crusher'

---
+++ repos/crusher-dev__crusher/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+git clone --recursive https://github.com/crusher-dev/crusher repo
+cd repo
+
+export DATABASE_URL=postgresql://postgres:password@localhost/postgres
+export MIGRATION_FOLDER=packages/crusher-server/prisma/migrations
+
+find $MIGRATION_FOLDER -iname "*.sql" ! -iname "down.sql" | sort | xargs printf -- '-f %s\n' | xargs psql $DATABASE_URL -v ON_ERROR_STOP=1
