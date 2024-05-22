36a1843d0385a91fcb953600a654bc7eabd7f1f0
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-22 12:31:39 +0100

    Feat: Add repo 'letterpad/letterpad'

---
+++ repos/letterpad__letterpad/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+git clone --recursive https://github.com/letterpad/letterpad repo
+cd repo
+
+export DATABASE_URL=../database.db
+export MIGRATION_FOLDER=apps/admin/prisma/sqlite/migrations
+
+find $MIGRATION_FOLDER -iname "*.sql" ! -iname "down.sql" | sort | while read file; do
+  sqlite3 $DATABASE_URL < $file
+done
