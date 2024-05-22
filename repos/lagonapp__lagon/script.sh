521d281699eed258dcdbed880ca3c631429b164e
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-22 15:34:25 +0100

    Feat: Add repo 'lagonapp/lagon'

---
+++ repos/lagonapp__lagon/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+git clone --recursive https://github.com/lagonapp/lagon repo
+cd repo
+
+export MIGRATION_FOLDER=packages/dashboard/prisma/migrations
+
+find $MIGRATION_FOLDER -iname "*.sql" ! -iname "down.sql" | sort | while read file; do
+  mariadb -uroot -ppassword db < $file
+done
