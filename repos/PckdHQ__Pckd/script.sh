d2987d00ffdbcb22273a6bd0a1fbef2e8dd43363
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-22 15:34:25 +0100

    Feat: Add repo 'PckdHQ/Pckd'

---
+++ repos/PckdHQ__Pckd/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+git clone --recursive https://github.com/PckdHQ/Pckd repo
+cd repo
+
+export MIGRATION_FOLDER=server/prisma
+
+export SCHEMA_FOLDER=server/prisma
+mkdir -p $SCHEMA_FOLDER/migrations/0_init
+prisma migrate diff \
+--from-empty \
+--to-schema-datamodel $SCHEMA_FOLDER/schema.prisma \
+--script > $SCHEMA_FOLDER/migrations/0_init/migration.sql
+
+find $MIGRATION_FOLDER -iname "*.sql" ! -iname "down.sql" | sort | while read file; do
+  mariadb -uroot -ppassword db < $file
+done
