fa98862c41060e03a874b912c4af59b57aa12b61
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-22 12:31:39 +0100

    Feat: Add repo 'zeekrey/teini'

---
+++ repos/zeekrey__teini/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+git clone --recursive https://github.com/zeekrey/teini repo
+cd repo
+
+export DATABASE_URL=../database.db
+export MIGRATION_FOLDER=prisma/migrations
+
+find $MIGRATION_FOLDER -iname "*.sql" ! -iname "down.sql" | sort | while read file; do
+  sqlite3 $DATABASE_URL < $file
+done
