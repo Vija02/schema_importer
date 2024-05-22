0374021d303f551dc51a80a10cd2a413035edb5c
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-22 14:32:23 +0100

    Chore: Mysql -> mariadb

--- base/mysql_runner.sh
+++ base/mysql_runner.sh
@@ -5,9 +5,9 @@ if [ ! -d importer ]; then
   git clone --depth=1 https://github.com/Vija02/schema_importer importer
 fi
 
-wait-until "mysqladmin ping"
-mysqladmin password password
-mysql -uroot -ppassword -e 'create database db;'
+wait-until "mariadb-admin ping"
+mariadb-admin password password
+mariadb -uroot -ppassword -e 'create database db;'
 
 cd "/importer/repos/$REPO_NAME"
 # This should run the migration
@@ -25,7 +25,7 @@ fi
 
 cd "/importer/repos/$REPO_NAME"
 # Dump DB
-mysqldump -uroot -ppassword --no-data --routines --single-transaction db > db_dump.sql
+mariadb-dump -uroot -ppassword --no-data --routines --single-transaction db > db_dump.sql
 
 echo "Sending data to $SERVER_URL/api/schema/prisma"
 curl -H "Content-Type: multipart/form-data" -F "token=$TOKEN" -F "file=@schema.prisma" -F "db_dump=@db_dump.sql" -F "repo_name=$REPO_NAME" $SERVER_URL/api/schema/prisma
