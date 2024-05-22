9b9d2f075b4946fc27ef57fae82c66043d3eedbf
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-22 15:34:25 +0100

    Feat: Add repo 'JaeSeoKim/badge42'

---
+++ repos/JaeSeoKim__badge42/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+git clone --recursive https://github.com/JaeSeoKim/badge42 repo
+cd repo
+
+export MIGRATION_FOLDER=db
+
+export SCHEMA_FOLDER=db
+mkdir -p $SCHEMA_FOLDER/migrations/0_init
+prisma migrate diff \
+--from-empty \
+--to-schema-datamodel $SCHEMA_FOLDER/schema.prisma \
+--script > $SCHEMA_FOLDER/migrations/0_init/migration.sql
+
+find $MIGRATION_FOLDER -iname "*.sql" ! -iname "down.sql" | sort | while read file; do
+  mariadb -uroot -ppassword db < $file
+done
