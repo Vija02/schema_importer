877c2c33129f635ce22e3996df3a7de5b08826de
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-22 15:34:25 +0100

    Feat: Add repo 'nikolovlazar/v1.nikolovlazar.com'

---
+++ repos/nikolovlazar__v1.nikolovlazar.com/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+git clone --recursive https://github.com/nikolovlazar/v1.nikolovlazar.com repo
+cd repo
+
+export MIGRATION_FOLDER=prisma/migrations
+
+find $MIGRATION_FOLDER -iname "*.sql" ! -iname "down.sql" | sort | while read file; do
+  mariadb -uroot -ppassword db < $file
+done
