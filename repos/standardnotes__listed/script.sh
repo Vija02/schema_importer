5a132ded09f5682d7d3d673bdd15583d8cbb2233
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-23 21:01:28 +0100

    Feat: Add repo 'standardnotes/listed'

---
+++ repos/standardnotes__listed/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+git clone --recursive https://github.com/standardnotes/listed repo
+
+cp repo/db /dsl_rails_placeholder/ -R
+cd /dsl_rails_placeholder
+
+sed -i 's/utf8mb4_0900_ai_ci/utf8_general_ci/g' db/schema.rb
+sed -i 's/CHARSET=utf8mb4/CHARSET=utf8/g' db/schema.rb
+sed -i 's/ COLLATE=utf8mb4_unicode_ci//g' db/schema.rb
+
+if test -f "db/schema.rb"; then
+  rails db:schema:load
+else
+	rails db:migrate
+fi
