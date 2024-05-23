1e6773b9ff6db4cf974f037532a91d73d792a9da
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-23 21:01:29 +0100

    Feat: Add repo 'wilsontheory/HyperLearn'

---
+++ repos/wilsontheory__HyperLearn/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+git clone --recursive https://github.com/wilsontheory/HyperLearn repo
+
+cp repo/db /dsl_rails_placeholder/ -R
+cd /dsl_rails_placeholder
+
+if test -f "db/schema.rb"; then
+  rails db:schema:load
+else
+	rails db:migrate
+fi
