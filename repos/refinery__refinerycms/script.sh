25a9fb711833e2a0a9c5a5aedd131ea2af95b978
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-23 21:34:45 +0100

    Feat: Update refinerycms

--- repos/refinery__refinerycms/script.sh
+++ repos/refinery__refinerycms/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 git clone --recursive https://github.com/refinery/refinerycms repo
 
-cp repo/db /dsl_rails_placeholder/ -R
+cp repo/pages/db /dsl_rails_placeholder/ -R
 cd /dsl_rails_placeholder
 
 if test -f "db/schema.rb"; then
