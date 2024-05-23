39d4f678ceb7291e8c7dee4fb167eb6bff3002a2
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-23 21:41:14 +0100

    Feat: Solidus

--- repos/solidusio__solidus/script.sh
+++ repos/solidusio__solidus/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 git clone --recursive https://github.com/solidusio/solidus repo
 
-cp repo/db /dsl_rails_placeholder/ -R
+cp repo/core/db /dsl_rails_placeholder/ -R
 cd /dsl_rails_placeholder
 
 if test -f "db/schema.rb"; then
