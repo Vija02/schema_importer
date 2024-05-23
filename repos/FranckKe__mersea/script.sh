bd56029287aff1ccf7acb921615bdf8fa1e8e6d6
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-23 11:13:22 +0100

    Feat: FranckKe/mersea

---
+++ repos/FranckKe__mersea/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+git clone https://github.com/FranckKe/mersea repo
+cd repo
+
+export DB_DATABASE=postgres 
+export DB_USERNAME=postgres 
+export DB_HOSTNAME=localhost 
+export DATABASE_PORT=5432 
+export DB_PASSWORD=postgres 
+export DATABASE_URL=postgresql://postgres:password@localhost/postgres 
+
+export RAILS_ENV=development
+
+sed -i -e "s/mersea_development/postgres/g" config/database.yml
+
+bundle install
+rails db:schema:load
