921f1425b5734ec7ef234ec0189a0bd18437034c
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-29 12:09:15 +0100

    Feat: lachlanjc/predictcovid

---
+++ repos/lachlanjc__predictcovid/script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+set -x
+git clone --recursive https://github.com/lachlanjc/predictcovid repo
+cd repo
+
+export DATABASE_URL=../database.db
+export MIGRATION_FOLDER=api/prisma
+
+cd api/prisma
+prisma format
+cd ../..
+
+export SCHEMA_FOLDER=api/prisma
+mkdir -p $SCHEMA_FOLDER/migrations/0_init
+prisma migrate diff \
+--from-empty \
+--to-schema-datamodel $SCHEMA_FOLDER/schema.prisma \
+--script > $SCHEMA_FOLDER/migrations/0_init/migration.sql
+
+mariadb -uroot -ppassword db < $SCHEMA_FOLDER/migrations/0_init/migration.sql
