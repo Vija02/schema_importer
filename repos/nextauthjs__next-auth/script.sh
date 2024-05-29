c2a1d059f158edd344f8be662693f9f101209423
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-29 12:12:36 +0100

    Chore: Fix next auth

--- repos/nextauthjs__next-auth/script.sh
+++ repos/nextauthjs__next-auth/script.sh
@@ -3,8 +3,13 @@ git clone --recursive https://github.com/nextauthjs/next-auth repo
 cd repo
 
 export DATABASE_URL=../database.db
-export MIGRATION_FOLDER=apps/dev/nextjs/prisma/migrations
+export MIGRATION_FOLDER=apps/dev/nextjs/prisma
 
-find $MIGRATION_FOLDER -iname "*.sql" ! -iname "down.sql" | sort | while read file; do
-  sqlite3 $DATABASE_URL < $file
-done
+export SCHEMA_FOLDER=apps/dev/nextjs/prisma
+mkdir -p $SCHEMA_FOLDER/migrations/0_init
+prisma migrate diff \
+--from-empty \
+--to-schema-datamodel $SCHEMA_FOLDER/schema.prisma \
+--script > $SCHEMA_FOLDER/migrations/0_init/migration.sql
+
+sqlite3 $DATABASE_URL < $SCHEMA_FOLDER/migrations/0_init/migration.sql
