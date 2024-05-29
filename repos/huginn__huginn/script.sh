bec7a82b28c02a010d59e4fe7bbef36be3a0f9d7
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-29 12:12:49 +0100

    Chore: Fix huginn

--- repos/huginn__huginn/script.sh
+++ repos/huginn__huginn/script.sh
@@ -1,11 +1,33 @@
 #!/bin/bash -e
+set -x
 git clone --recursive https://github.com/huginn/huginn repo
+cd repo
 
-cp repo/db /dsl_rails_placeholder/ -R
-cd /dsl_rails_placeholder
+cat <<EOT > .env
+APP_SECRET_TOKEN=REPLACE_ME_NOW!
+DOMAIN=localhost:3000
+PORT=3000
+FORCE_SSL=false
 
-if test -f "db/schema.rb"; then
-  rails db:schema:load
-else
-	rails db:migrate
-fi
+DATABASE_ADAPTER=postgresql
+DATABASE_ENCODING=utf8
+DATABASE_RECONNECT=true
+DATABASE_NAME=postgres
+DATABASE_POOL=20
+DATABASE_USERNAME=postgres
+DATABASE_PASSWORD=postgres
+DATABASE_PORT=5432
+DATABASE_HOST=localhost
+EOT
+
+export DATABASE_NAME=postgres 
+export DATABASE_USERNAME=postgres 
+export DATABASE_HOST=localhost 
+export DATABASE_PORT=5432 
+export DATABASE_PASSWORD=postgres 
+
+export RAILS_ENV=production
+
+bundle install
+bundle add bigdecimal
+bundle exec rake db:migrate
