703c28611547aa4599dc2f1776f2631b0f918db1
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-30 23:32:01 +0100

    Feat: Fix more

--- repos/ClaperCo__Claper/script.sh
+++ repos/ClaperCo__Claper/script.sh
@@ -11,7 +11,10 @@ export DATABASE_PASSWORD=postgres
 export DATABASE_URL=postgresql://postgres:password@localhost/postgres 
 
 export MIX_ENV=prod
+export BASE_URL=http://test.com
 export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
 
+mix deps.update phoenix_live_dashboard
+
 mix deps.get --force
-mix ecto.setup
+mix ecto.migrate
