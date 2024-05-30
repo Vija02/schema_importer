f2fff387a1e6cb6aa96b8183eab38b6eeeb051b1
Michael Salim <salim_michaelmi@yahoo.com>
2024-05-30 14:50:08 +0100

    Feat: Update more

--- repos/faisalsayed10__firefiles/script.sh
+++ repos/faisalsayed10__firefiles/script.sh
@@ -2,8 +2,11 @@
 git clone --recursive https://github.com/faisalsayed10/firefiles repo
 cd repo
 
-export MIGRATION_FOLDER=apps/web/prisma/migrations
+export SCHEMA_FOLDER=apps/web/prisma
+mkdir -p $SCHEMA_FOLDER/migrations/0_init
+prisma migrate diff \
+--from-empty \
+--to-schema-datamodel $SCHEMA_FOLDER/schema.prisma \
+--script > $SCHEMA_FOLDER/migrations/0_init/migration.sql
 
-find $MIGRATION_FOLDER -iname "*.sql" ! -iname "down.sql" | sort | while read file; do
-  mariadb -uroot -ppassword db < $file
-done
+mariadb -uroot -ppassword db < $SCHEMA_FOLDER/migrations/0_init/migration.sql
--- repos/pluralsh__plural/script.sh
+++ repos/pluralsh__plural/script.sh
@@ -2,6 +2,7 @@
 git clone https://github.com/pluralsh/plural repo
 
 cd repo
+git pull
 
 apt install -y erlang-dev
 
@@ -10,6 +11,18 @@ sed -i -e "s/0.8.5/0.8.7/g" mix.exs
 # https://stackoverflow.com/questions/76562092/hi-i-had-created-elixir-project-with-phoenix-framework-there-is-yaml-file-when
 sed -i -e "s/3.9.0/3.10/g" mix.exs
 
+export DATABASE_NAME=postgres 
+export DATABASE_USER=postgres 
+export DATABASE_HOST=localhost 
+export DATABASE_PORT=5432 
+export DATABASE_PASSWORD=postgres 
+export DATABASE_URL=postgresql://postgres:password@localhost/postgres 
+
+export MIX_ENV=dev
+export SECRET_KEY_BASE=BVPgT+mzxjDi6FiG4f4+6mxqE731gDxpT5kw0fp1tAs5taquyZ39kOEUbQBlm4Od
+
+sed -i -e "s/forge_dev/postgres/g" config/dev.exs
+
 mix deps.update ssl_verify_fun prometheus_ex
 mix deps.get --force
-DATABASE_NAME=postgres DATABASE_USER=postgres DATABASE_HOST=localhost DATABASE_PORT=5432 DATABASE_PASSWORD=postgres DATABASE_URL=postgresql://postgres:password@localhost/postgres mix ecto.setup
+mix ecto.setup
